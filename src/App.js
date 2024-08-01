c7c84562b40ae59d06adb18b41a40083658fef09
MuratY2 <murat.yazgan@ozu.edu.tr>
2024-08-02 02:44:55 +0300

    try workflow

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to TercihMeta</h1>
+        <h1>Welcome to TercihMeta bro</h1>
         <p>Your college decision-making assistant!</p>
       </header>
     </div>
